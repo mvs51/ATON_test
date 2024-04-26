8d306e190776955af18f599dacbc7a4f390c5edb
Maria Subbotina <maria.subbotina@aramcoinnovations.com>
2024-04-26 16:04:38 +0300

    Form and request to finmarket

--- currency_info/currency/urls.py
+++ currency_info/currency/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('currency_info/', views.get_currency, name='currency_info')
 ]
